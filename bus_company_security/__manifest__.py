647e9c29147571f6b12febc252290ef7ed933db0
Henrik Norlin <henrik.norl@gmail.com>
2021-10-28 12:47:08 +0200

    [ADD] bus_company_security

---
+++ bus_company_security/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 AppsToGROW - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Bus Company Security',
+
+    'author': 'AppsToGROW',
+    'auto_install': True,
+    'category': 'Uncategorized',
+    'depends': ['bus', 'base_company_security'],
+    'license': 'AGPL-3',
+    'version': '14.0.1.0.0',
+    'website': 'http://www.appstogrow.co',
+}
