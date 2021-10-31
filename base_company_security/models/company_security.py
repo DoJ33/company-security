0298b589fd28d673658b5cda0a6649c092bcf914
Henrik Norlin <henrik.norl@gmail.com>
2021-10-31 22:04:41 +0100

    [DEV] base_company_security: _set_global_security_rules_on_all_models_except_ir_rule() works well

--- base_company_security/models/company_security.py
+++ base_company_security/models/company_security.py
@@ -3,7 +3,6 @@ from openupgradelib import openupgrade
 import os
 
 from odoo import models, fields, api, exceptions, SUPERUSER_ID
-from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
@@ -12,13 +11,13 @@ _logger = logging.getLogger(__name__)
 # Used for company_id fields, also in hooks.py.
 EXTID_MODULE_NAME = '__base_company_security__'
 
-PUBLIC_MODELS = [
+PUBLIC_MODEL = [
     'res.users',
     'website',
     'website.menu',
 ]
 
-SYSTEM_COMPANY_MODELS = [
+SYSTEM_COMPANY_MODEL = [
     'account.account.type',
     'ir.actions.act_url',
     'ir.actions.act_window',
@@ -43,7 +42,7 @@ SYSTEM_COMPANY_MODELS = [
     'uom.uom',
 ]
 
-SYSTEM_MODELS = [
+SYSTEM_MODEL = [
     # BASE, IR, RES
     'base.language.export',
     'base.language.import',
@@ -92,7 +91,7 @@ SYSTEM_MODELS = [
     'wizard.ir.model.menu.create',
 ]
 
-NO_ACCESS_MODELS = [
+NO_ACCESS_MODEL = [
     'ir.config_parameter',
 ]
 
@@ -100,60 +99,79 @@ NO_ACCESS_MODELS = [
 #     # 'view_name': 'model_name',
 # }
 
-# NEW, NOT TESTED #####################################################################################
+# NEW #####################################################################################
 
-SECURITY_RULES = {
-    'PUBLIC_MODELS': {
-        'read_if': 'allowed',
-        'edit_if': 'allowed AND selected_company/parent/child',
+SECURITY_RULE = {
+    'PUBLIC_MODEL': {
+        'read_if': 'allowed_companies',
+        'edit_if': 'allowed_companies AND selected_company/parent/child',
     },
     # default
-    'COMPANY_MODELS': {
-        'read_if': 'allowed AND selected_company/parent/child',
-        'edit_if': 'allowed AND selected_company/parent/child',
+    'COMPANY_MODEL': {
+        'read_and_edit_if': 'allowed_companies AND selected_company/parent/child',
     },
-    'SYSTEM_COMPANY_MODELS': {
-        'read_if': 'system OR ( allowed AND selected_company/parent/child )',
-        'edit_if': 'allowed AND selected_company/parent/child',
+    'SYSTEM_COMPANY_MODEL': {
+        'read_if': 'system_company OR ( allowed_companies AND selected_company/parent/child )',
+        'edit_if': 'allowed_companies AND selected_company/parent/child',
     },
-    'SYSTEM_MODELS': {
-        'read_if': 'system',
-        'edit_if': 'system AND ( allowed AND selected_company )',
+    'SYSTEM_MODEL': {
+        'read_if': 'system_company',
+        'edit_if': 'system_company AND ( allowed_companies AND selected_company )',
     },
-    'NO_ACCESS_MODELS': {
-        'read_if': 'system AND ( allowed AND selected_company )',
-        'edit_if': 'system AND ( allowed AND selected_company )',
+    'NO_ACCESS_MODEL': {
+        'read_and_edit_if': 'system_company AND ( allowed_companies AND selected_company )',
     },
 }
 
-SECURITY_DOMAIN_WORDS = {
+SECURITY_DO_IF = {
+    'read_if': {
+        'perm_read': True,
+        'perm_write': False,
+        'perm_create': False,
+        'perm_unlink': False,
+    },
+    'edit_if': {
+        'perm_read': False,
+        'perm_write': True,
+        'perm_create': True,
+        'perm_unlink': True,
+    },
+    'read_and_edit_if': {
+        'perm_read': True,
+        'perm_write': True,
+        'perm_create': True,
+        'perm_unlink': True,
+    },
+}
+
+SECURITY_DOMAIN_WORD = {
     '(': 'BEGIN',
     ')': 'END',
-    'AND': "&",
-    'OR': "|",
-    'allowed': "('{company_id}','in',company_ids)",
+    'AND': "'&'",
+    'OR': "'|'",
+    'allowed_companies': "('{company_id}','in',company_ids)",
     'selected_company': "('{company_id}','=',company_id)",
     'selected_company/parent/child': "'|',('{company_id}','child_of',company_id),('{company_id}','parent_of',company_id)",
-    'system': "('{company_id}','=',1)",
+    'system_company': "('{company_id}','=',1)",
 }
 
-SECURITY_FIELDS = {
+COMPANY_FIELD = {
     'res.company': 'id',
     'res.users': 'company_ids',
     'default': 'company_id',
 }
 
 def _get_security_type(model_name):
-    if model_name in PUBLIC_MODELS:
-        return 'public'
-    elif model_name in SYSTEM_COMPANY_MODELS:
-        return 'system_company'
-    elif model_name in SYSTEM_MODELS:
-        return 'system'
-    elif model_name in NO_ACCESS_MODELS:
-        return 'no_access'
+    if model_name in NO_ACCESS_MODEL:
+        return 'NO_ACCESS_MODEL'
+    elif model_name in SYSTEM_MODEL:
+        return 'SYSTEM_MODEL'
+    elif model_name in SYSTEM_COMPANY_MODEL:
+        return 'SYSTEM_COMPANY_MODEL'
+    elif model_name in PUBLIC_MODEL:
+        return 'PUBLIC_MODEL'
     else:
-        return 'company'
+        return 'COMPANY_MODEL'
 
 def _assert_security_domain_words_and_order(words_list):
     type = ''
@@ -162,7 +180,7 @@ def _assert_security_domain_words_and_order(words_list):
     first = 0
     last = len(words_list) - 1
     for count, word in enumerate(words_list):
-        assert word in SECURITY_DOMAIN_WORDS
+        assert word in SECURITY_DOMAIN_WORD, word + " not in " + str(words_list)
 
         if word == '(':
             type = 'parenthesis'
@@ -184,40 +202,43 @@ def _assert_security_domain_words_and_order(words_list):
     # There should be max one operator type inside a parenthesis.
     # This assert is done in the _recursive_words2domain method.
 
-def _recursive_words2domain(words_list):
-    words_sub_list = []
-    domain_list = []
+def _recursive_order_words(words_list):
+    words_sub_list = {}
     parenthesis_counter = 0
     operator = ''
     last_operator = ''
     for word in words_list:
         if word == '(':
             if parenthesis_counter > 0:
-                words_sub_list.append(word)
+                words_sub_list[parenthesis_counter].append(word)
             parenthesis_counter += 1
         elif word == ')':
             parenthesis_counter -= 1
             if parenthesis_counter > 0:
-                words_sub_list.append(word)
+                words_sub_list[parenthesis_counter].append(word)
             else:
-                domain_list.extend(_recursive_words2domain(words_sub_list))
+                words_sub_list.setdefault(parenthesis_counter, []).append(_recursive_order_words(words_sub_list[1]))
+                words_sub_list[1] = []
                 last_operator = ''
         else:
             if word in ('AND', 'OR'):
                 operator = word
                 assert operator == last_operator or not last_operator
-            words_sub_list.append(word)
+            words_sub_list.setdefault(parenthesis_counter, []).append(word)
     words_sub_dict = {}
-    for count, word in enumerate(words_sub_dict):
+    for count, word in enumerate(words_sub_list[0]):
         if count % 2 == 0:
             words_sub_dict[count+1] = word
         else:
             words_sub_dict[count-1] = word
-
-    for count in range(0, len(words_sub_dict)):
+    ordered_words_list = []
+    for count in range(0, len(words_sub_dict) + 1):
         if count in words_sub_dict:
-            domain_list.append(SECURITY_DOMAIN_WORDS[words_sub_dict[count]])
-    return domain_list
+            if type(words_sub_dict[count]) is list:
+                ordered_words_list.extend(words_sub_dict[count])
+            else:
+                ordered_words_list.append(words_sub_dict[count])
+    return ordered_words_list
 
 
 class CompanySecurity(models.AbstractModel):
@@ -227,53 +248,106 @@ class CompanySecurity(models.AbstractModel):
     # main methods
 
     def _register_hook(self):
-        self._limit_access_to_system_models()
+        # Returning an error value will be ignored (see loading.py).
+        self._remove_access_to_NO_ACCESS_MODEL()
+        self._remove_edit_access_to_SYSTEM_MODEL()
         self._set_global_security_rules_on_all_models_except_ir_rule()
         self._set_company_manager_access_and_rules()
 
-    def _limit_access_to_system_models(self):
+    def _remove_access_to_NO_ACCESS_MODEL(self):
+        pass
+
+    def _remove_edit_access_to_SYSTEM_MODEL(self):
         pass
 
     def _set_global_security_rules_on_all_models_except_ir_rule(self):
-        models = self['ir.models'].search([('model', '!=', 'ir.rule')])
+        models = self.env['ir.model'].search([('model', '!=', 'ir.rule')])
         for model in models:
-            security_type = _get_security_type(model._name)
-            for do_if in SECURITY_RULES[security_type]:
-                domain = self._words2domain(do_if)
-                # values = rule_dictionary
-                # self._set_security_rule(values)
+            SECURITY_TYPE = _get_security_type(model.model)
+            for do_if, domain_words in SECURITY_RULE[SECURITY_TYPE].items():
+                values = SECURITY_DO_IF[do_if]
+                values['groups'] = []
+                values['model_id'] = model.id
+                values['domain_force'] = self._words2domain(words=domain_words, model=model.model)
+                values['name'] = '{model} - {security_type}, {do_if}'.format(
+                    model=model.model, security_type=SECURITY_TYPE.lower(), do_if=do_if
+                )
+                self._set_security_rule(values)
 
     def _set_company_manager_access_and_rules(self):
         pass
 
     # low-level methods
 
-    def _words2domain(self, words):
+    def _words2domain(self, words, model):
         words_list = words.split(' ')
         _assert_security_domain_words_and_order(words_list)
-        domain_list = _recursive_words2domain(words_list)
-        domain_draft = ', '.join(domain_list)
-
-        domain = domain_draft.format(company_id='company_id')
+        ordered_words_list = _recursive_order_words(words_list)
+        ordered_domain_list = [SECURITY_DOMAIN_WORD[word] for word in ordered_words_list]
+        domain_draft = "[{}]".format(', ' . join(map(str, ordered_domain_list)))
+        if model in COMPANY_FIELD:
+            company_field = COMPANY_FIELD[model]
+        else:
+            company_field = COMPANY_FIELD['default']
+        domain = domain_draft.format(company_id=company_field)
         return domain
 
+    def _set_security_rule(self, values):
+        Rule = self.env['ir.rule']
+        domain = [('name', '=', values['name']), ('model_id', '=', values['model_id'])]
+        log_critical = False
+        # For groups, only allow command 4 adding an existing group
+        group_ids = [g[1] for g in values['groups'] if g[0] == 4]
+        if group_ids:
+            domain.append(('groups', 'in', group_ids))
+        else:
+            domain.append(('global', '=', True))
+        # Search for a rule with same name, model and groups.
+        # Update the rule or create a new rule.
+        rule = Rule.search(domain)
+        if len(rule) > 1:
+            # Try to deduplicate ...
+            rule_ids = rule.ids
+            rule_values = {}
+            for r in rule:
+                rule_values[r.id] = {'domain_force': r.domain_force, 'perm_read': r.perm_read, 'perm_write': r.perm_write, 'perm_create': r.perm_create, 'perm_unlink': r.perm_unlink}
+            for id in rule_ids:
+                if not rule_values[id] == rule_values[rule_ids[0]]:
+                    log_critical = True
+            if not log_critical:
+                rule = Rule.browse(rule_ids.pop(0))
+                Rule.browse(rule_ids).unlink()
+            else:
+                # ... or log a critical error.
+                model = self.env['ir.model'].browse(values['model_id']).model
+                groups = [group.name for group in self.env['res.groups'].browse(group_ids)]
+                _logger.critical('There are {count} rules with name="{name}", model="{model}" and groups={groups} and different settings!'.format(
+                    count=len(rule), name=values['name'], model=model, groups=groups
+                ))
+        if len(rule) == 1:
+            v = values
+            if not (rule.name == v['name'] and rule.model_id.id == v['model_id'] and rule.domain_force == v['domain_force'] and rule.perm_read == v['perm_read'] and rule.perm_write == v['perm_write'] and rule.perm_create == v['perm_create'] and rule.perm_unlink == v['perm_unlink']):
+                rule.write(values)
+        elif len(rule) == 0 and not log_critical:
+            rule = self.env['ir.rule'].create(values)
 
 # OLD #####################################################################################
 
-SECURITY_TYPES = [
-    'company',
-    'company_published',
-    'public',
-    'system',
-    'system_company',
-    'users',
+    SECURITY_TYPES_OLD = [
+        'company',
+        'company_published',
+        'public',
+        'system',
+        'system_company',
+        'users',
+    ]
 
-    def _register_hook(self):
+    def _register_hook_old(self):
         self._protect_system_models()
         self._create_security_rules()
         self._update_company_manager_access_and_rules()
 
-    def _protect_system_models(self):
+    def _protect_system_models_old(self):
         system_model_ids = self.env['ir.model'].search([('model', 'in', SYSTEM_MODELS)]).ids
         system_group_ids = (self.env.ref('base.group_erp_manager').id,self.env.ref('base.group_system').id)
 
@@ -297,7 +371,7 @@ SECURITY_TYPES = [
         if access_records:
             raise UserError('These records give access to system models: %s' % access_records)
 
-    def _create_security_rules(self):
+    def _create_security_rules_old(self):
         values_for_rules = self._get_security_rules()
         for values in values_for_rules:
             values['perm_read'] = values.pop('read')
@@ -330,7 +404,7 @@ SECURITY_TYPES = [
                 )
         self._set_company_id_to_1_where_null()
 
-    def _update_company_manager_access_and_rules(self):
+    def _update_company_manager_access_and_rules_old(self):
         Rule = self.env['ir.rule']
         Access = self.env['ir.model.access']
         company_manager_group = self.env.ref('base_company_security.group_company_manager')
@@ -378,7 +452,7 @@ SECURITY_TYPES = [
             else:
                 access = Access.create(values)
 
-    def _get_security_rules(self, models=None):
+    def _get_security_rules_old(self, models=None):
         # return list of dict (name, model, domain, read, write, create, unlink)
         model_type = self._get_model_security_type(models)
         domain = self._get_security_domains()
@@ -413,7 +487,7 @@ SECURITY_TYPES = [
 
         return sorted(rule, key = lambda k:k['model'])
 
-    def _get_model_security_type(self, models=None, *args, **kwargs):
+    def _get_model_security_type_old(self, models=None, *args, **kwargs):
 
         views = self._get_and_fix_name_and_find_model_of_all_sql_views()
         if not models:
@@ -449,7 +523,7 @@ SECURITY_TYPES = [
                                     Cancelling _get_model_security_type""" % (my_type, my_model, str(SECURITY_TYPES)))
         return type
 
-    def _get_security_domains(self):
+    def _get_security_domains_old(self):
 
         # system (no access)
         # users                                                           (company_ids*)
@@ -477,7 +551,7 @@ SECURITY_TYPES = [
         return domain
 
     # Security risk? YES!, if users can set company-specific rules ...
-    def _set_rule_values(self, values):
+    def _set_rule_values_old(self, values):
         Rule = self.env['ir.rule']
         rule = Rule.search([('name','=',values['name'])])
         if len(rule) > 1:
@@ -493,7 +567,7 @@ SECURITY_TYPES = [
             rule = self.env['ir.rule'].create(values)
         return rule
 
-    def _get_and_fix_name_and_find_model_of_all_sql_views(self):
+    def _get_and_fix_name_and_find_model_of_all_sql_views_old(self):
         # Get views
         self.env.cr.execute("select table_name from information_schema.views where table_schema = 'public';")
         views = [v[0] for v in self.env.cr.fetchall()]
@@ -512,7 +586,7 @@ SECURITY_TYPES = [
 
         return views_fixed
 
-    def _set_company_id_to_1_where_null(self):
+    def _set_company_id_to_1_where_null_old(self):
         _logger.debug('set_company_id_to_1_where_null: start')
         self.env.cr.execute("SELECT t.table_name FROM information_schema.tables t INNER JOIN information_schema.columns c ON t.table_name = c.table_name WHERE t.table_type='BASE TABLE' AND c.column_name='company_id' ORDER BY table_name;")
         tables = self.env.cr.fetchall()
