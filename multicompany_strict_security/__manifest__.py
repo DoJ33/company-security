9823320b614380d15cc506ff7f3b5502859c9010
Henrik Norlin <henrik.norl@gmail.com>
2021-11-25 20:16:41 +0100

    res.company JSON field (lowercase)

--- multicompany_strict_security/__manifest__.py
+++ multicompany_strict_security/__manifest__.py
@@ -1,6 +1,11 @@
 # Copyright 2021 AppsToGROW - Henrik Norlin
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+# Copyight
+# https://gist.github.com/danmana/5242f37b7d63daf4698de7c61c8b59fc#file-json_field-js
+# https://www.odoo.com/forum/help-1/best-way-to-show-json-data-on-odoo-ui-171100
+# Check the last link; I have asked if the person would like to make an Odoo module out of this.
+
 {
             'name': 'multicompany_strict_security',
             'summary': 'Patch, Create database, Install',
@@ -10,6 +15,7 @@
             'data': [
                 'security/ir.model.access.csv',
                 'security/security.xml',
+                'views/json_field.xml',
                 'views/views.xml',
             ],
             'depends': [
@@ -24,10 +30,25 @@
             'description': '''
 APPLY PATCHES
 cd /path/to/odoo-server
-git am /path/to/extid-with-company_id.patch
-git am /path/to/users-company_dependent-partner.patch
+git apply /path/to/multicompany_strict_security/patches/*
 
-TODO: If res.users has multiple fields linking to res.partner, only partner_id should trigger read/edit in ir.property.
+
+TODO:
+
+Create new company: Return the new company (otherwise new mail.channel is added to the old company).
+Compare with "normal" behaviour without the strict security.
+
+If res.users has multiple fields linking to res.partner, only partner_id should trigger read/edit in ir.property.
 (odoo/osv/expression.py)
+
+When changing res.user company_id, the partner is also changing company_id! Should not change.
+
+Restrict so a specific partner can only belong to one user.
+
+Create one mail.channel per company, and define in res.company.json. (Done, but in the wrong company)
+Method self.env.ref('mail.channel_all_employees') should return the json value.
+
+It seems that JSON doesn't replace old values with new values...
+
             ''',
 }
